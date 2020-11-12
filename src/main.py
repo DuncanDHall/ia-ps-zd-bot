b3988dce646210e390e0f897c7c418e61033a0a4
Duncan Hall <duncandhall@gmail.com>
2020-11-12 12:02:52 -0500

    change to get

--- src/main.py
+++ src/main.py
@@ -14,7 +14,17 @@ def jsondata():
     return jsonify({"name": "duncan"}), 200
 
 
-@app.route("/consult.json, methods=['GET']")
+@app.route("/test", methods=['GET', 'POST'])
+def test_get():
+    if request.method == 'GET':
+        return jsonify({"job": "good"}), 200
+    elif request.method == 'POST':
+        print(">>>>", request.get_json())
+        return request.get_json()
+
+
+
+@app.route("/consult.json", methods=['GET'])
 def notify_consultant():
     print(request)
     return jsonify({"job": "good"}), 200
