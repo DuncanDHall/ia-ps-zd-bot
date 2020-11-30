23b137eefcb11b5359199c0c906f02dc35fb1925
Duncan Hall <duncandhall@gmail.com>
2020-11-30 15:53:43 -0500

    trying to get rid of large stuff

--- src/diffbot.py
+++ src/diffbot.py
@@ -8,18 +8,17 @@ from config import FROM_ZENDESK_FOLDER, FROM_ARCHIVE_ACCOUNTS_FOLDER, \
 
 
 def run():
-    # mail.change_folder([4, 5], folders[1], folders[2])
-    data = mail.get_msgs([FROM_ZENDESK_FOLDER, FROM_ARCHIVE_ACCOUNTS_FOLDER])
-    pickle.dump(data, open('maildata.pickle', 'wb'))
-    # data = pickle.load(open('maildata.pickle', 'rb'))
-    # zendesk_msgs = data[FROM_ZENDESK_FOLDER].values()
-    # support_msgs = data[FROM_ARCHIVE_ACCOUNTS_FOLDER].values()
-    # logger.debug('beginning matching process with {} zd and {} support msgs'.format(len(zendesk_msgs), len(support_msgs)))
-    # zendesk_unmatched, support_unmatched = diff.unmatched_msgs(zendesk_msgs, support_msgs)
-    # print("Unmatched from Zendesk: {}".format(len(zendesk_unmatched)))
-    # print("Unmatched from Support: {}".format(len(support_unmatched)))
-    #
-    # pickle.dump((zendesk_unmatched, support_unmatched), open('temp.pickle', 'wb'))
+    # data = mail.get_msgs([FROM_ZENDESK_FOLDER, FROM_ARCHIVE_ACCOUNTS_FOLDER])
+    # pickle.dump(data, open('maildata.pickle', 'wb'))
+    data = pickle.load(open('maildata.pickle', 'rb'))
+    zendesk_msgs = data[FROM_ZENDESK_FOLDER].values()
+    support_msgs = data[FROM_ARCHIVE_ACCOUNTS_FOLDER].values()
+    logger.debug('beginning matching process with {} zd and {} support msgs'.format(len(zendesk_msgs), len(support_msgs)))
+    zendesk_unmatched, support_unmatched = diff.unmatched_msgs(zendesk_msgs, support_msgs)
+    print("Unmatched from Zendesk: {}".format(len(zendesk_unmatched)))
+    print("Unmatched from Support: {}".format(len(support_unmatched)))
+
+    pickle.dump((zendesk_unmatched, support_unmatched), open('temp.pickle', 'wb'))
 
     # mail.change_folder(zendesk_unmatched, FROM_ZENDESK_FOLDER, UNMATCHED_ZENDESK_FOLDER)
     # mail.change_folder(support_unmatched, FROM_ARCHIVE_ACCOUNTS_FOLDER, UNMATCHED_ARCHIVE_FOLDER)
--- src/zdbotutils/diff.py
+++ src/zdbotutils/diff.py
@@ -1,7 +1,10 @@
 import difflib
 import datetime
+import io
 
 from tqdm import tqdm
+
+from zdbotutils.custom_logging import logger
 
 
 def unmatched_msgs(msgs1, msgs2):
@@ -10,7 +13,7 @@ def unmatched_msgs(msgs1, msgs2):
         msgs2,
         sort_key=lambda msg: msg[b'INTERNALDATE'],
         within_error=email_datetime_match,
-        is_match=email_match
+        is_match=zd_archive_email_match
     )
 
 
@@ -23,10 +26,6 @@ def diff_pools(aa, bb, sort_key, within_error, is_match):
     :param is_match: func whether items are match
     :return:
     """
-    ################
-    import pudb
-    # pudb.set_trace()
-    ################
     aa = sorted(aa, key=sort_key)
     bb = sorted(bb, key=sort_key)
     i = 0
@@ -36,8 +35,16 @@ def diff_pools(aa, bb, sort_key, within_error, is_match):
     b_matched = [False] * len(bb)
     a_unmatched = []
 
+    # ################
+    # import pudb
+    # pudb.set_trace()
+    # ################
+
     for i, a in tqdm(enumerate(aa), desc="Matching messages..."):
+        logger.debug('on zd email {}/{}: {}'.format(i, len(bb), a[b'ENVELOPE'].subject))
         while not within_error(a, bb[j0]):
+            if sort_key(a) < sort_key(bb[j0]):
+                break
             j0 += 1
 
         j = j0
@@ -59,30 +66,73 @@ def diff_pools(aa, bb, sort_key, within_error, is_match):
     return a_unmatched, b_unmatched
 
 
-def email_datetime_match(msg1, msg2, margin=datetime.timedelta(days=1)):
+def email_datetime_match(msg1, msg2, margin=datetime.timedelta(minutes=1)):
     d1 = msg1[b'INTERNALDATE']
     d2 = msg2[b'INTERNALDATE']
+    # logger.debug('time diff: {} | {} & {}'.format(d1 - d2, d2 - d1, margin))
+    # logger.debug('time diff: {} | {}'.format(d1 - d2 < margin, d2 - d1 < margin))
+    # logger.debug(msg1[b'ENVELOPE'].subject.decode())
+    # logger.debug(msg2[b'ENVELOPE'].subject.decode())
     return d1 - d2 < margin and d2 - d1 < margin
 
 
-def email_match(msg1, msg2):
-    if not from_match(msg1, msg2):
+def zd_archive_email_match(zd_msg, archive_msg):
+    if not from_match(zd_msg, archive_msg):
         return False
-    return text_match(msg1, msg2)
+    is_match = text_match(zd_msg, archive_msg)
+    return is_match
 
 
-def text_match(msg1, msg2, threshold=0.97):
-    matcher = difflib.SequenceMatcher(isjunk=lambda c: c in ' \n\r\t', autojunk=False)
-    matcher.set_seqs(msg1[b'BODY[TEXT]'], msg2[b'BODY[TEXT]'])
+def text_match(zd_msg, archive_msg, threshold=0.97):
+
+    zd_body_buf = io.BytesIO(zd_msg[b'BODY[TEXT]'])
+    while zd_body_buf.readline().strip() != b'': pass  # one blank line after headers
+    while zd_body_buf.readline().strip() != b'': pass  # one blank line after ZD annotation
+    zd_msg_body = zd_body_buf.read()
+
+    archive_body_buf = io.BytesIO(archive_msg[b'BODY[TEXT]'])
+    while archive_body_buf.readline().strip() != b'': pass  # one blank line after headers
+    archive_msg_body = archive_body_buf.read()
+
+    matcher = difflib.SequenceMatcher(isjunk=lambda c: c in b' \n\r\t', autojunk=False)
+    matcher.set_seqs(zd_msg_body, archive_msg_body)
+
+    ################
+    import pudb
+    pudb.set_trace()
+    ################
+    logger.debug("""MATCH?: 
+        {}
+        ********
+        {}
+        """.format(zd_msg_body, archive_msg_body))
+
+    # TODO
+    d1 = zd_msg[b'INTERNALDATE']
+    d2 = archive_msg[b'INTERNALDATE']
+    time_diff = d1 - d2
 
     if matcher.real_quick_ratio() < threshold:
+        logger.debug('real quick: {}, time_diff: {}, {}'.format(matcher.real_quick_ratio(), time_diff, archive_msg[b'ENVELOPE'].subject))
         return False
     if matcher.quick_ratio() < threshold:
+        logger.debug('quick: {}'.format(matcher.quick_ratio()))
         return False
-    return matcher.ratio() > threshold
+    if matcher.ratio() > threshold:
+        logger.debug("""MATCH FOUND {}: 
+        {}
+        ********
+        {}
+        """.format(matcher.ratio(), zd_msg_body, archive_msg_body))
+        return True
 
 
-def from_match(msg1, msg2):
-    from_1 = msg1[b'ENVELOPE'].from_[0]
-    from_2 = msg2[b'ENVELOPE'].from_[0]
-    return from_1.mailbox == from_2.mailbox and from_1.host == from_2.host
+def from_match(zd_msg, archive_msg):
+    return True
+
+    # archive_msg_from_address = archive_msg[b'ENVELOPE'].from_[0]
+    # archive_from = '{}@{}'.format(
+    #     archive_msg_from_address.mailbox, archive_msg_from_address.host
+    # ).lower()
+    #
+    # return zd_from == archive_from
